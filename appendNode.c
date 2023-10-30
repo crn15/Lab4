1fabb36b3ee0cef5bc7ae11a20d60f366479f120
crojas <candyrojas579@gmail.com>
2023-10-30 15:56:03 -0600

    Se agregaron comentarios.

--- appendNode.c
+++ appendNode.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Función para agregar un nodo al final de la lista
+// Función para agregar un nodo al final de la lista.
 void appendNode(struct Node** head, int value) {
+
 struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+
 newNode->data = value;
+
 newNode->next = NULL;
+
+// Con el condicional, si la lista está vacía, el nuevo nodo se convierte en la cabeza de la lista.
 if (*head == NULL) {
-*head = newNode; // Si la lista está vacía, el nuevo nodo se convierte en la cabeza de la lista.
-} else {
+*head = newNode;
+}
+else {
+
 struct Node* current = *head;
+
 while (current->next != NULL) {
 current = current->next;
 }
-current->next = newNode; // Agrega el nuevo nodo al final de la lista.
+
+// Aquì se agrega el nodo al final de la lista.
+current->next = newNode;
 }
 }
 

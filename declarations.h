3967f66818a53f818058226be1e5fe31171ee429
crojas <candyrojas579@gmail.com>
2023-10-29 19:07:09 -0600

    Funciòn que llama a las otras funciones y se agrega archivo con declaraciones.

---
+++ declarations.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Estructura para representar un nodo en la lista enlazada
+struct Node {
+int data;
+struct Node* next;
+};
+
+// Función para agregar un nodo al final de la lista
+void appendNode(struct Node** head, int value)
+
+// Función para eliminar un nodo por valor
+void deleteNode(struct Node** head, int value)
+
+// Función para agregar un nodo al inicio de la lista
+void prependNode(struct Node** head, int value)
+
+// Función para buscar un nodo por valor e imprimir si existe o no
+void searchNode(struct Node* head, int value)
+
+// Función para imprimir la lista
+void printList(struct Node* head)
+
+// Función para obtener el elemento enésimo e imprimir su contenido
+void getNodeAtIndex(struct Node* head, int index)
+
+// Función para liberar la memoria de la lista
+void freeList(struct Node* head)
---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+struct Node* head = NULL;
+
+int num;
+printf("INGRESE LA CANTIDAD DE ELEMENTOS A AGREGAR A LA LISTA: ");
+scanf("%d", &num);
+
+for (int i=0; i<num; i++) {
+	int element;
+	printf("INGRESE EL VALOR DEL ELEMENTO: ");
+	scanf("%d", &element);
+
+	head= appendNode(&head, element);}
+
+printf("Lista inicial:\n");
+printList(head);
+
+//AQUI VAN LOS EJEMPLOS DE CÒMO FUNCIONA EL CÒDIGO.
+
+// Agrega un nodo al inicio de la lista (valor 65).
+prependNode(&head, 65);
+printf("Lista después de agregar 65 al inicio:\n");
+printList(head);
+
+// Agrega un nodo al final de la lista (valor 83)
+appendNode(&head, 83);
+printf("Lista después de agregar 83 al final:\n");
+printList(head);
+
+// Elimina un nodo por valor (valor 65)
+deleteNode(&head, 65);
+printf("Lista después de eliminar el valor 65:\n");
+printList(head);
+
+// Busca un nodo por su valor (valor 65 y 83)
+searchNode(head, 65);
+searchNode(head, 83);
+
+// Obtiene y muestra el enesimo elemento de la lista.
+getNodeAtIndex(head, 7);
+
+// Libera la memoria al final del programa
+freeList(head);
+
+return 0;
+}
+

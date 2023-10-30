052fae0a8d70a23c00fc93def839d24500b660c0
crojas <candyrojas579@gmail.com>
2023-10-30 16:08:59 -0600

    Se agregaron comentarios.

--- getNode.c
+++ getNode.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Función para obtener el elemento enésimo e imprimir su contenido
+// Función para buscar un nodo a partir de un indice.
 void getNode(struct Node* head, int index) {
+
 int count = 0;
 struct Node* current = head;
+
+//Este bucle busca el nodo en la lista.
 while (current != NULL) {
 if (count == index) {
 printf("Elemento en el índice %d: %d\n", index, current->data);
@@ -13,6 +16,7 @@ return;
 count++;
 current = current->next;
 }
+
 printf("No existe un elemento en el índice %d.\n", index);
 }
 
